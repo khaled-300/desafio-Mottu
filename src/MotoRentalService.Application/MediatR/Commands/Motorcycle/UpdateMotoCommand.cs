cc9ece61a3bb060fce3f33b185b10d1465329b93
KHALED AL JADAAN <kh.aljadaan@gmail.com>
2024-05-23 17:41:49 -0300

    excluding Id parameter on openapi doc

--- src/MotoRentalService.Application/MediatR/Commands/Motorcycle/UpdateMotoCommand.cs
+++ src/MotoRentalService.Application/MediatR/Commands/Motorcycle/UpdateMotoCommand.cs
@@ -1,10 +1,12 @@
 ﻿using MediatR;
 using MotoRentalService.Application.MediatR.Response.Moto;
+using MotoRentalService.Domain.Aggregates;
 
 namespace MotoRentalService.Application.MediatR.Commands.Motorcycle
 {
     public class UpdateMotoCommand : IRequest<MotorcycleCommandResult>
     {
+        [SwaggerExclude]
         public int Id { get; set; }
         public string LicensePlate { get; set; }
     }
